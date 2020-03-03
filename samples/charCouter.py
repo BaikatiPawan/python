68335bafbea420462eefeb5316e296fea7cc5162
BaikatiPawan <pavank4190@gmail.com>
2020-03-03 18:05:00 +0530

    charcounter.py

---
+++ samples/charCouter.py
@@ -0,0 +1,13 @@
+def char_Occurrence(str):
+    dict={}
+    for ch in str:
+        keys = dict.keys()
+        if (ch.isspace())==True:
+                continue
+        if ch in keys:
+            dict[ch]+=1
+        else:
+            dict[ch]=1
+    return dict
+
+print(char_Occurrence("priyan   kaa"))
